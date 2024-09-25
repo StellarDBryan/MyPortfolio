1fbfe1ed4a2226597c5a82e9be4dadcca0f339b6
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-09-24 18:01:12 -0600

    Add porfolio skeleton in page.js

--- app/page.js
+++ app/page.js
@@ -3,99 +3,73 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Aqui va la Portada y descripción */}
+      <section>
+        <div>
+          <p>Bryan Sigala</p>
+          <p>Hi I'm Bryan Sigala, a Full-Stack Developer from Chihuahua, Mexico. I love having challenges and building new projects</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <button>Contact Me</button>
+      </section>
+      <section>
+
+        {/* Aqui va el About Me  y Tools/Skills */}
+        <div>
+          <p>About Me</p>
+          <p>My name is Bryan Aaron Sigala Jaime, I'm a Intern from the Computing Systems Engineer Career specialized as a Full-Stack Developer. </p>
+        </div>
+        <div>
+            <p>Skills</p>
+            <ul>
+              <li>- Java </li>
+              <li>- SQL Databases </li>
+              <li>- Figma </li>
+              <li>- OOP</li>
+              <li>- React.js </li>
+            </ul>
+          </div>
+        
+      </section>
+
+      {/* Aqui van los Projects */}
+      <section>
+
+          <div>
+            <p>Projects</p>
+            <ul>
+              <li>
+                <p>- UniTalent Project </p>
+                <p>FEB-MAY 2024</p>
+                <p>Me and some friends build an app for Job Oportunities to Interns, so the enterprises don't have difficulties on contact to the students. </p>
+                <ul>
+                  <li>- Used Next.js for the Frontend. </li>
+                  <li>- Used TailwindCSS for the responsive design of the app. </li>
+                  <li>- Design UI prototypes in Figma. </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          
+        </section>
+          {/* Aqui va la Experience */}
+        <section>
+
+        <div>
+          <p>Experiencie</p>
+          <ul>
+            <li>
+              <p>Encora Spark Internship Program</p>
+              <p>JUN-AUG 2024</p>
+              <p>I was member of the 2024B batch of the Internchip Program and participate in some activities for Full-Stack Development. </p>
+              <ul>
+                <li>- Used Java with Spring Boot for Backend programming. </li>
+                <li>- Used React.js for Frontend programming. </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+
+        </section>
     </div>
   );
 }
