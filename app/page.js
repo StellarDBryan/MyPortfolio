5996084c03a6d391c877e29af2e05c2534f7293a
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-09-25 15:45:27 -0600

    Add vortex.jsx file to components/ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Vortex } from "@/components/vortex";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Aqui va la Portada y descripción */}
       <section>
+        <Vortex />
         <div>
           <h1>Bryan Sigala</h1>
           <p>Hi I'm Bryan Sigala, a Full-Stack Developer from Chihuahua, Mexico. I love having challenges and building new projects</p>
