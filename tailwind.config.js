5cc16bbeeec7ea008b938f264f0edd60afe68e4c
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-09-28 20:10:23 -0600

    Try BoxesCore component in UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,10 @@
+// Code for backgroud-boxes component
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,24 +12,41 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       }, 
-      fontSize: {
-        'text-h1': '2.986rem', 
-        'text-h2': '2.488rem',
-        'text-h3': '2.074rem', 
-        'text-h4': '1.728rem', 
-        'text-h5': '1.44rem', 
-        'text-h6': '1.2rem',  
-        'text-p': '1rem',  
-        'text-small-1': '0.833rem', 
-        'text-small-2': '0.694rem',   
-      },
+    },
+    fontSize: {
+      'h1': '2.986rem', 
+      'h2': '2.488rem',
+      'h3': '2.074rem', 
+      'h4': '1.728rem', 
+      'h5': '1.44rem', 
+      'h6': '1.2rem',  
+      'p': '1rem',  
+      'small-1': '0.833rem', 
+      'small-2': '0.694rem',   
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+// Code for backgroud-boxes component
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({
+  addBase,
+  theme
+}) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
+}
